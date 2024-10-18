afaf5d720a68c1135703460039fea3c070214a27
Frenadol <fernandoramajimenez@gmail.com>
2024-10-18 10:23:11 +0200

    Archivos iniciales

---
+++ src/main/java/com/github/Frenadol/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.github.Frenadol;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
