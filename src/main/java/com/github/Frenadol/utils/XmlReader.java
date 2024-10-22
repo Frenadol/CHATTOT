35c1ff071e98c43f2c593e4a3c90263374d5728c
Hancelmito <juandavid.dominguez004@gmail.com>
2024-10-22 23:26:06 +0200

    arreglo de los usuarios

--- src/main/java/com/github/Frenadol/utils/XmlReader.java
+++ src/main/java/com/github/Frenadol/utils/XmlReader.java
@@ -4,6 +4,10 @@ import com.github.Frenadol.model.User;
 import org.w3c.dom.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,5 +49,40 @@ public class XmlReader {
         }
 
         return users;
+    }
+
+    public static void saveUsersToXML(List<User> users, String filePath) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            Element root = document.createElement("users");
+            document.appendChild(root);
+
+            for (User user : users) {
+                Element userElement = document.createElement("user");
+
+                Element name = document.createElement("username");
+                name.appendChild(document.createTextNode(user.getName()));
+                userElement.appendChild(name);
+
+                Element image = document.createElement("image");
+                String imageBase64 = java.util.Base64.getEncoder().encodeToString(user.getProfileImagen());
+                image.appendChild(document.createTextNode(imageBase64));
+                userElement.appendChild(image);
+
+                root.appendChild(userElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(filePath));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
